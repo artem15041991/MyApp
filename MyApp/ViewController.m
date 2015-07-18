8420753591783768c7b2d03a337d25653d3e1ca6
Artem Karpov <artem15041991@gmail.com>
2015-07-18 15:35:09 +0600

    Set label text by textField text.

--- MyApp/ViewController.m
+++ MyApp/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+
+@property (strong, nonatomic) IBOutlet UITextField *testTextField;
+
+@property (strong, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
 
@@ -19,9 +23,13 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)adjustButtonAction:(id)sender {
+    self.testLabel.text = self.testTextField.text;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
